cd7e09a4b92da3371dfe044a51c3e0247fc0105e
Radiy <notron12460@gmail.com>
2025-10-26 12:58:37 +0000

    Все обернуто в отдельные функции, добавлены пояснения

--- hw2/task1/reverse_str.asm
+++ hw2/task1/reverse_str.asm
@@ -5,7 +5,7 @@ global _start
 section .data
     write_msg db "Write some string: ", 0
     write_msg_len  equ $ - write_msg
-    err_msg db "Error writing to stdout", 10
+    err_msg db "Error reading stdin", 10
     err_msg_len equ $ - err_msg
 
 section .bss
@@ -13,26 +13,42 @@ section .bss
 
 section .text
 _start:
-    ; Чтение из stdin
+    jmp read_stdin
+
+; Чтение из stdin
+read_stdin:
     mov rax, 0                  ; 0 - sys_read
     mov rdi, 0                  ; 0 - stdin
     mov rsi, input              ; передаем указатель на массив, в который попадут введенные данные
     mov rdx, 1024 
     syscall
 
-    ; Запись в stdout
+    test rax, rax               ; Проверка на корректное вовзращенное значение
+    js exit_with_error
+
+    jmp echo_stdout
+
+; Запись в stdout
+echo_stdout:
     mov rdx, rax                ; Передать кол-во прочитанных байт в rdx для вывода
     mov rax, 1                  ; 1 - sys_write
     mov rdi, 1                  ; 1 - stdout
     mov rsi, input              ; Передать указатель на input буффер
     syscall
 
-    ; Выход
+    cmp rdx, rax                ; Проверить, что все данные были записаны в stdout
+    jnz exit_with_error
+    
+    jmp exit
+
+; Выход
+exit:
     mov rax, 60
     mov rdi, 0                  ; Возвращаем 0 при успешном завершении
     syscall
 
-handle_error:
+; Выход из программы с ошибкой
+exit_with_error:
     mov rax, 1
     mov rdi, 2
     mov rsi, err_msg
