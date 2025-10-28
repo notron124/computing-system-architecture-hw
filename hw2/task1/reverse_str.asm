a10e3a86522e9505be5c9d156db847019c194f86
Radiy <notron12460@gmail.com>
2025-10-28 14:23:42 +0500

    Чтение из stdin и переворот строки обернуты в функции

--- hw2/task1/reverse_str.asm
+++ hw2/task1/reverse_str.asm
@@ -14,14 +14,41 @@ section .bss
 
 section .text
 _start:
-    jmp read_stdin
+    mov rbx, input
+    mov rcx, 1024
+    call read_stdin
 
-; Чтение из stdin
+    mov rdi, input                  ; Передаем указатель на массив, содержащий исходную строку
+    mov rsi, reversed_str           ; Передаем указатель на массив, куда попадет перевернутая строка
+    mov rdx, rax                    ; Передаем размер исходной строки
+    
+    mov r9, rax                     ; Сохраняем размер исходной строки
+
+    call flip_string
+    
+    cmp r9, rax
+    jnz exit_with_error
+
+    mov rsi, reversed_str
+    mov rdx, rax
+
+    call write_stdout
+
+    jmp exit
+
+; --- read_stdin ---
+; @brief    Фукнция для чтения из stdin
+; @param    rbx - указатель на массив, в который будут записаны данные
+; @param    rcx - размер переданного массива
+; @return   rax - размер записанных в массив данных
+; @note     Аналог на Си: int read_stdin(char* input, size_t input_size)
+; @note     Проверяет rax на содержание кода ошибки (отрицательное число) после
+; @note     чтения из stdin
 read_stdin:
     mov rax, 0                      ; 0 - sys_read
     mov rdi, 0                      ; 0 - stdin
-    mov rsi, input                  ; передаем указатель на массив, в который попадут введенные данные
-    mov rdx, 1024 
+    mov rsi, rbx                    ; передаем указатель на массив, в который попадут введенные данные
+    mov rdx, rcx 
     syscall
 
     test rax, rax                   ; Проверка на корректное вовзращенное значение (>=0)
@@ -30,58 +57,80 @@ read_stdin:
     cmp byte [input], 0x0A          ; Проверка на строку, стостоящую только из перевода каретки 
     je empty_string_warning         ; Переход, если первый знак в input == 0x0A
 
-    mov rbx, rax                    ; Сохраняем длину строки
-    mov rcx, rbx                    ; Счетчик от конца строки
-    dec rcx                         ; Игнорируем перевод каретки
-
-    jmp flip_string
+    ret
 
 ; Сообщение о пустой строке
 empty_string_warning:
-    mov r9, err_empty_str           ; Передаем указатель на строку с информацтей об ошибке 
-    mov r10, err_empty_str_len      ; Передаем ее длину
+    mov rsi, err_empty_str          ; Передаем указатель на строку с информацей об ошибке 
+    mov rdx, err_empty_str_len      ; Передаем ее длину
     
-    call write_stdout                ; Вызываем функцию записи в stdout
+    call write_stdout               ; Вызываем функцию записи в stdout
 
     jmp read_stdin                  ; Повторяем попытку получить корректную строку от пользователя
+; --- read_stdin ---
 
-; Разворот строки
+; --- flip_string ---
+; @brief    Фукнция для "переворота" строки
+; @param    rdi - указатель на массив исходных данных
+; @param    rsi - указатель на массив, куда будет записана "перевенутая" строка
+; @param    rdx - размер исходных данных
+; @return   rax - размер записанных в массив, переданный в rsi, данных
+; @note     Аналог на Си: int flip_string(char* input, char* output, size_t input_size)
+; @note     Проверяет, что rdx больше 1, иначе возвращает код ошибки -1
+
+; Инициализация необходимых значений
 flip_string:
+    cmp rdx, 1                      ; Если размер переданных данных меньше 1
+    jl exit_flip_string_with_error  ; выходим с ошибкой
+
+    mov rcx, rdx                    ; Инициализируем счетчик, который будет указывать на текущий последний знак в строке
+    dec rcx                         ; Пропускаем перевод каретки
+    xor r9, r9                      ; обнуляем r9, так как будем исользовать
+                                    ; его в качестве счетчика обработанный байт
+
+; "Переворот" данных
+flip_string_loop:
     dec rcx                         ; уменьшаем счетчик, который указывает на конец input
-    js terminate_string             ; если он стал отрицательным, значит инвесия закончена - выходим
+    js end_flip_string              ; если он стал отрицательным, значит инвесия закончена - выходим
 
-    mov al, [input + rcx]           ; Получаем последний знак на данный момент 
+    mov al, [rdi + rcx]             ; Получаем последний знак на данный момент 
                                     ; al - младший байт регистра RAX, в данном случае необходим, так как данные длиной 1 байт 
-    mov [reversed_str + r8], al     ; помещаем последний знак в начало rersed_str
-    inc r8
+    mov [rsi + r9], al              ; помещаем последний знак в начало reversed_str
+    inc r9
 
-    jmp flip_string                 ; Повторяем, пока не обработаем все знаки
+    jmp flip_string_loop            ; Повторяем, пока не обработаем все знаки
 
-; Добавить перевод каретки в конец строки
-terminate_string: 
+; Вставка перевода каретки в конец перевернутой строки
+end_flip_string: 
     mov al, 10
-    mov [reversed_str + r8], al     ; Перевод каретки
-    inc r8
+    mov [reversed_str + r9], al     ; Перевод каретки
+    inc r9
 
-    mov r9, reversed_str
-    mov r10, r8
+    mov rax, r9
 
-    call write_stdout
-    
-    jmp exit                        ; Проверка на ошибку записи в stdout
-                                    ; происходит в функции записи, так что тут можно не проверять
+    ret
 
-; Запись в stdout
+exit_flip_string_with_error:
+    mov rax, -1
+    ret
+; --- flip_string ---
+
+; --- write_stdout ---
+; @brief    Фукнция для записи в stdout
+; @param    r9 - указатель на буфер, из которого будут извлечены данные
+; @param    r10 - количество байт, которые необходимо записать в stdout
+; @return   rax - размер записанных в stdout данных
+; @note     Аналог на Си: int write_stdout(char* data, size_t input_size)
+; @note     Проверяет rax на соответствие r9 после записи в stdout
 write_stdout:
     mov rax, 1                      ; 1 - sys_write
     mov rdi, 1                      ; 1 - stdout
-    mov rsi, r9                     ; Передать указатель на reserved_str буффер
-    mov rdx, r10                    ; Передать размер прочитанной строки
     syscall
 
     cmp rdx, rax                    ; Проверить, что все данные были записаны в stdout
     jnz exit_with_error
     ret
+; --- write_stdout ---
 
 ; Выход
 exit:
